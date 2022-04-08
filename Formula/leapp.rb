81d2775da53bd4a1a4c28d52e50d3aef13c2f6a2
Marco Vanetti <marco.vanetti@gmail.com>
2022-04-08 10:52:35 +0200

    first commit

---
+++ Formula/leapp.rb
@@ -0,0 +1,16 @@
+class LeappCli < Formula
+  desc "Install Leapp CLI"
+  homepage "https://leapp.cloud"
+  url "https://leapp-cli-test.s3.eu-west-1.amazonaws.com/versions/0.1.8/a7c5fd1c/leapp-v0.1.8-a7c5fd1c-darwin-x64.tar.xz"
+  sha256 "136187f31e7573179c0926fcdbcc316a7d371b128c91982f83fdc64f839faf0c"
+
+  def install
+    inreplace "bin/leapp", /^CLIENT_HOME=/, "export LEAPP_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/leapp"
+  end
+
+  test do
+    system bin/"leapp", "version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+Leapp CLI Homebrew Tap
+-------------------
+
+Install the Leapp CLI with
+
+    $ brew install Noovolari/leapp/cli
+
+For more information, visit https://leapp.cloud
