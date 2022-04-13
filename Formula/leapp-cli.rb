08064011b032e695eac3fd0cfa24c93120721576
Marco Vanetti <marco.vanetti@gmail.com>
2022-04-13 17:59:57 +0200

    no message

--- Formula/leapp-cli.rb
+++ Formula/leapp-cli.rb
@@ -2,7 +2,7 @@ class LeappCli < Formula
   desc "Install Leapp CLI"
   homepage "https://leapp.cloud"
   url "https://leapp-cli-test.s3.eu-west-1.amazonaws.com/versions/leapp-v0.1.12-1ff8a8e8-darwin-x64.tar.xz"
-  sha256 "aa41b6b85914e461248764f7efb00975e371339785073d15bc420b42cd25451d"
+  sha256 "3cde053bb43e6a6b61a8da752754c31da226ffde963a8c4eae3ca81814f79177"
 
   def install
     inreplace "bin/leapp", /^CLIENT_HOME=/, "export LEAPP_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
