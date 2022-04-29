37e6ec2a1e922eadde27c42101e192be3665374b
Marco Vanetti <marco.vanetti@gmail.com>
2022-04-29 16:36:57 +0200

    trying homebrew npm

--- Formula/leapp-cli.rb
+++ Formula/leapp-cli.rb
@@ -1,14 +1,18 @@
+require "language/node"
+
 class LeappCli < Formula
   desc "Install Leapp CLI"
   homepage "https://leapp.cloud"
-  version "0.1.18"
-  url "https://leapp-cli-test.s3.eu-west-1.amazonaws.com/versions/leapp-v0.1.18-675237b-darwin-x64.tar.xz"
-  sha256 "e5e67cb1253ba88767f19fc458990f40396b88707896ca9ab3b85120d764b92e"
+  version "0.1.19"
+  url "https://registry.npmjs.org/@noovolari/leapp-cli/-/leapp-cli-0.1.12.tgz"
+  sha256 "38303fc14b0177f4592c0213598e0b86fc982bf7777bc9f9a2723f134e1dd9bc"
+
+  depends_on "node@16"
+  depends_on "python" => :build
 
   def install
-    inreplace "bin/leapp", /^CLIENT_HOME=/, "export LEAPP_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
-    libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/leapp"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
