5651f26bef71fe3e226d8f6160a6615085836877
Marco Vanetti <marco.vanetti@gmail.com>
2022-05-13 15:06:01 +0200

    attempt

--- Formula/leapp-cli.rb
+++ Formula/leapp-cli.rb
@@ -3,13 +3,13 @@ require "language/node"
   class LeappCli < Formula
   desc "Install Leapp CLI"
   homepage "https://leapp.cloud"
-  version "0.1.27"
+  version "0.1.28"
   url "https://registry.npmjs.org/@lerna-test-forge/leapp-cli/-/leapp-cli-0.1.26.tgz"
   sha256 "e8cead87724fd06e4502d30f60802a288147497e956712996d34e8bf55f5077b"
 
   depends_on "node@16"
-  depends_on "libsecret"
-  depends_on "python" => :build
+  #depends_on "libsecret"
+  #depends_on "python" => :build
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec).reject { |a| a == "--build-from-source" }
