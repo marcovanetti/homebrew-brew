a7b7cd54e18c246ad6a25cf1a393361bd8da85cd
Marco Vanetti <marco.vanetti@gmail.com>
2022-05-13 15:00:18 +0200

    prebuild mode for npm install

--- Formula/leapp-cli.rb
+++ Formula/leapp-cli.rb
@@ -3,7 +3,7 @@ require "language/node"
   class LeappCli < Formula
   desc "Install Leapp CLI"
   homepage "https://leapp.cloud"
-  version "0.1.26"
+  version "0.1.27"
   url "https://registry.npmjs.org/@lerna-test-forge/leapp-cli/-/leapp-cli-0.1.26.tgz"
   sha256 "e8cead87724fd06e4502d30f60802a288147497e956712996d34e8bf55f5077b"
 
@@ -12,7 +12,7 @@ require "language/node"
   depends_on "python" => :build
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec).reject { |a| a == "--build-from-source" }
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
