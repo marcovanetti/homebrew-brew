d63a0958610a44258f338c6441eaeb2d02ace09d
Marco Vanetti <marco.vanetti@gmail.com>
2022-06-30 15:32:31 +0200

    Update leapp-cli.rb

--- Formula/leapp-cli.rb
+++ Formula/leapp-cli.rb
@@ -7,7 +7,7 @@ class LeappCli < Formula
   url "https://registry.npmjs.org/@lerna-test-forge/leapp-cli/-/leapp-cli-0.1.44.tgz"
   sha256 "ab7e541a8c193fe8e282c8498f4a4b4c39cb46f39dc98781a945ce7c408e5b00"
 
-  depends_on "node@16"
+  depends_on "node"
   depends_on "libsecret"
   depends_on "python" => :build
 
