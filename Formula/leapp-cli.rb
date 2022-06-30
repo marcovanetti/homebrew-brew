d13d0398e2b42a41c7c57eeeac48095ed6c569ac
Marco Vanetti <marco.vanetti@gmail.com>
2022-06-30 15:37:54 +0200

    Update leapp-cli.rb

--- Formula/leapp-cli.rb
+++ Formula/leapp-cli.rb
@@ -3,7 +3,7 @@ require "language/node"
 class LeappCli < Formula
   desc "Install Leapp CLI"
   homepage "https://leapp.cloud"
-  version "0.1.45"
+  version "0.1.45
   url "https://registry.npmjs.org/@lerna-test-forge/leapp-cli/-/leapp-cli-0.1.44.tgz"
   sha256 "ab7e541a8c193fe8e282c8498f4a4b4c39cb46f39dc98781a945ce7c408e5b00"
 
